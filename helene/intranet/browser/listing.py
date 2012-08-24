fe02eeb4314ea2be2817e6599fe71ffe46a6ce2a
tdesvenain <thomas.desvenain@gmail.com>
2012-08-24 17:46:59 +0200

    viewlets & listing

--- helene/intranet/browser/listing.py
+++ helene/intranet/browser/listing.py
@@ -1,18 +1,24 @@
 from collective.edm.listing.listingoptions import DefaultListingOptions
 from collective.edm.listing.listingrights import DefaultListingRights
 
-class HeleneListingRights(DefaultListingRights):
+class ListingRights(DefaultListingRights):
+    """ """
 
     def globally_show_author(self):
         """View cut column
         """
         return False
 
+    def globally_can_delete(self, brains):
+        if not self.context.isTrashcanOpened():
+            return False
 
-class HeleneListingOptions(DefaultListingOptions):
+        return super(ListingOptions, self).globally_can_delete()
+
+
+class ListingOptions(DefaultListingOptions):
 
     sort_mode = 'auto'
     default_sort_on = 'sortable_title'
     default_sort_order = 'asc'
     allow_edit_popup = True
-
